ba74c1b4ee70d776a216ba5ba717ab8916a6ad66
homdalhmod199@gmail.com <hmodalhmod199@gmail.com>
2020-08-20 16:13:01 +0300

    start a now branch

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,7 +19,10 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect(RouteServiceProvider::HOME);
+
+            return $guard == 'admin' ? redirect(RouteServiceProvider::ADMIN) : redirect(RouteServiceProvider::HOME);
+
+
         }
 
         return $next($request);
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,6 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/home';
+    public const ADMIN = '/admin';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -46,6 +47,11 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
+
+        $this->mapSiteRoutes();
+
+        $this->mapAdminRoutes();
+
         //
     }
 
@@ -61,6 +67,21 @@ class RouteServiceProvider extends ServiceProvider
         Route::middleware('web')
             ->namespace($this->namespace)
             ->group(base_path('routes/web.php'));
+    }
+
+    protected function mapAdminRoutes()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)->prefix('admin')
+            ->group(base_path('routes/admin.php'));
+    }
+
+
+    protected function mapSiteRoutes()
+    {
+        Route::middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/site.php'));
     }
 
     /**
