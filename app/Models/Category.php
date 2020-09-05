23a71b44b52bf9684200bedcaa124d292492eed5
homdalhmod199@gmail.com <hmodalhmod199@gmail.com>
2020-09-05 20:24:22 +0300

    finish settings

---
+++ app/Models/Category.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use Translatable;
+    protected $with = ['translations'];
+
+
+    protected $translatedAttributes = ['name'];
+
+
+    protected $fillable = ['parent_id', 'slug', 'is_active'];
+    protected $hidden = ['translations'];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+
+}
